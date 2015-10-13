4a26b2e9c545645514d6d569c4ce4768f1167e70
vibhu0009 <cvibhu0009@gmail.com>
2015-10-13 20:23:59 +0200

    [4/4] Screen recorder by 0xD34D

--- config/common.mk
+++ config/common.mk
@@ -99,6 +99,11 @@ PRODUCT_PACKAGES += \
     LockClock \
     OmniSwitch
 
+# Screen recorder
+PRODUCT_PACKAGES += \
+    ScreenRecorder \
+    libscreenrecorder
+
 #    SlimFileManager removed until updated
 # Extra tools
 PRODUCT_PACKAGES += \
